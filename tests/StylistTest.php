3196b85e630397496719ec10dedb8ccbf7ef4d63
sarah leahy <sarah leahy>
2017-02-23 21:29:21 -0700

    create save and get all tests

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -12,4 +12,38 @@
     $DB = new PDO($server, $username, $password);
 
     class StylistTest extends PHPUnit_Framework_TestCase
-    {}
+    {
+        function test_getAll()
+        {
+            $stylist_name = "Debbie";
+            $id = 1;
+            $stylist_test = new Stylist($stylist_name, $id);
+            $stylist_test->save();
+
+            $stylist_name = "Tom";
+            $id = 2;
+            $stylist_test = new Stylist($stylist_name, $id);
+            $stylist_test->save();
+            //Act
+            $result = Stylist::getAll();
+            //Assert
+            $this->assertEquals($result[0],$stylist_test);
+        }
+        function test_save()
+       {
+           //Arrange
+           $stylist_name = "Debbie";
+           $id = 1;
+           $stylist_test = new Stylist($stylist_name, $id);
+           $stylist_test->save();
+
+           $stylist_name = "Tom";
+           $id = 2;
+           $stylist_test = new Stylist($stylist_name, $id);
+           $stylist_test->save();
+           //Act
+           $result = Stylist::getAll();
+           //Assert
+           $this->assertEquals($result[0],$stylist_test);
+       }
+    }
